676a8fbb5145e229a67f93b3e227a6a97ec7024e
Bruno Miguel <brunomiguel0798@gmail.com>
2024-11-21 22:12:17 +0000

    Refactor Zombie class usage and improve memory management
    - Corrected the static method call for newZombie in main.cpp
    - Added null check before calling announce and deleting the zombie
    - Ensured proper memory management by deleting dynamically allocated zombies
    - Maintained code consistency and followed best practices

--- CPP01/ex00/main.cpp
+++ CPP01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bsousa-d <bsousa-d@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 12:32:06 by bsousa-d          #+#    #+#             */
-/*   Updated: 2024/11/14 13:09:14 by bsousa-d         ###   ########.fr       */
+/*   Updated: 2024/11/21 22:08:54 by bsousa-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,13 @@ int main()
     std::cout << "Enter name:";
     std::cin >> zombieName;
 
-    zombie = zombie->newZombie(zombieName);
+    zombie = Zombie::newZombie(zombieName);
     randomChump(zombieName);
-    zombie->announce();
+	if (zombie != NULL)
+	{
+    	zombie->announce();
+    	delete zombie;
+	}
 
-    delete zombie;
     return 0;
 }
