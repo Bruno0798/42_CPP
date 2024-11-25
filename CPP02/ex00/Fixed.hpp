4debe0b9d39a9726dbd072dfe54be6cb6175b01c
Bruno0798 <brunomiguel0798@gmail.com>
2024-11-25 18:37:39 +0000

    Started CPP02

---
+++ CPP02/ex00/Fixed.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsousa-d <bsousa-d@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 18:26:36 by bsousa-d          #+#    #+#             */
+/*   Updated: 2024/11/25 18:26:36 by bsousa-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed {
+private:
+	int _value;
+	static const int _number;
+public:
+	Fixed();
+	Fixed(const Fixed& other);
+	Fixed& operator=(const Fixed& other);
+	~Fixed();
+};
+
+#endif
