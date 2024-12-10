0e1c3c5fa94b98c4642a61482c8d3500d0a8ec59
Bruno Miguel <brunomiguel0798@gmail.com>
2024-12-10 18:13:00 +0000

    Preparing ex00 for deliver

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: bsousa-d <bsousa-d@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 18:34:16 by bsousa-d          #+#    #+#             */
-/*   Updated: 2024/11/25 19:38:25 by bsousa-d         ###   ########.fr       */
+/*   Updated: 2024/12/10 18:06:27 by bsousa-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,36 +15,36 @@
 
 Fixed::Fixed()
 {
-	std::cout << << "Default Constructor Called" << std::endl;
+	std::cout << YELLOW << "Default Constructor Called" << RESET << std::endl;
 	this->_value = 0;
 }
 
 Fixed::Fixed(const Fixed &other) : _value()
 {
-	std::cout << "Copy Constructor Called" << std::endl;
+	std::cout << "Copy Constructor Called" << RESET << std::endl;
 	*this = other;
 }
 
 Fixed &Fixed::operator=(const Fixed &other)
 {
-	std::cout << "Copy Assignment Operator Called" << std::endl;
+	std::cout << "Copy Assignment Operator Called" << RESET << std::endl;
 	_value = other.getRawBits();
 	return *this;
 }
 
 Fixed::~Fixed()
 {
-	std::cout << RED << "Destructor Called" << std::endl;
+	std::cout << RED << "Destructor Called" << RESET << std::endl;
 }
 
 void Fixed::setRawBits(const int raw)
 {
-	std::cout << "setRawBits Member Function Called" << std::endl;
+	std::cout << "setRawBits Member Function Called" << RESET << std::endl;
 	this->_value = raw;
 }
 
 int Fixed::getRawBits() const
 {
-	std::cout << "getRawBits Member Function Called" << std::endl;
+	std::cout << "getRawBits Member Function Called" << RESET << std::endl;
 	return _value;
 }
--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bsousa-d <bsousa-d@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 18:20:28 by bsousa-d          #+#    #+#             */
-/*   Updated: 2024/11/25 18:20:28 by bsousa-d         ###   ########.fr       */
+/*   Updated: 2024/12/10 18:12:24 by bsousa-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,27 +33,41 @@
 
 int main()
 {
+	system("clear");
 	std::cout << BLUE << "Creating a default Fixed object:" << RESET << std::endl;
 	Fixed a;
 	std::cout << GREEN << "a.getRawBits(): " << a.getRawBits() << RESET << std::endl;
+	std::cout << BLUE << "\nPress Enter to continue..." << RESET << std::endl;
+	std::cin.get();  // Waits for Enter key
+	system("clear");
 
-	std::cout << BLUE << "\nCreating a copy of Fixed object a:" << RESET << std::endl;
+	std::cout << BLUE << "Creating a copy of Fixed object a:" << RESET << std::endl;
 	Fixed b(a);
 	std::cout << GREEN << "b.getRawBits(): " << b.getRawBits() << RESET << std::endl;
+	std::cout << BLUE << "\nPress Enter to continue..." << RESET << std::endl;
+	std::cin.get();  // Waits for Enter key
+	system("clear");
 
-	std::cout << BLUE << "\nAssigning Fixed object a to a new Fixed object c:" << RESET << std::endl;
+	std::cout << BLUE << "Assigning Fixed object a to a new Fixed object c:" << RESET << std::endl;
 	Fixed c;
 	c = a;
 	std::cout << GREEN << "c.getRawBits(): " << c.getRawBits() << RESET << std::endl;
+	std::cout << BLUE << "\nPress Enter to continue..." << RESET << std::endl;
+	std::cin.get();  // Waits for Enter key
+	system("clear");
 
-	std::cout << BLUE << "\nSetting raw bits of Fixed object a to 42:" << RESET << std::endl;
+	std::cout << BLUE << "Setting raw bits of Fixed object a to 42:" << RESET << std::endl;
 	a.setRawBits(42);
 	std::cout << GREEN << "a.getRawBits(): " << a.getRawBits() << RESET << std::endl;
+	std::cout << BLUE << "\nPress Enter to continue..." << RESET << std::endl;
+	std::cin.get();  // Waits for Enter key
+	system("clear");
 
-	std::cout << BLUE << "\nCreating a new Fixed object d and setting its raw bits to 100:" << RESET << std::endl;
+	std::cout << BLUE << "Creating a new Fixed object d and setting its raw bits to 100:" << RESET << std::endl;
 	Fixed d;
 	d.setRawBits(100);
 	std::cout << GREEN << "d.getRawBits(): " << d.getRawBits() << RESET << std::endl;
+	std::cout << std::endl;
 
 	return 0;
 }
